7a856448dc1476444b64f837ae7948a3db62ccce
Nahuel de la Vega <nahueldlvega@gmail.com>
2022-06-24 16:25:30 -0300

    cambios en app.js de registro

--- js/app.js
+++ js/app.js
@@ -4,33 +4,31 @@ const register = (event) =>{
     let name = document.querySelector('#register-name').value;
     let lastName = document.querySelector('#register-lastname').value;
     let email = document.querySelector('#register-email').value;
-    // let age = document.getElementById('register-age').value;
     let pass= document.getElementById('register-password').value;
     let pass2= document.getElementById('register-password-confirm').value;
 
-    let nameOk = /^([a-zA-Z]){4,35}$/.test(name); //true
-    let lastNameOk = /^([a-zA-Z]){4,35}$/.test(lastName); //true
-    let emailOk= /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(email); //true
-    // let ageOk = age>=16 && age<150; //true
-    let passOk= /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(pass); //true
-    let pass2Ok= /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(pass2); //true
-    let equalPass = pass === pass2; //true
+    let nameCheck = /^([a-zA-Z]){4,35}$/.test(name); 
+    let lastNameCheck = /^([a-zA-Z]){4,35}$/.test(lastName);
+    let emailCheck= /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(email);
+    let passCheck= /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(pass);
+    let pass2Check= /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(pass2);
+    let idemPass = pass === pass2;
 
-    if(name  && lastNameOk && emailOk /*&& ageOk*/ && pass2Ok && passOk && equalPass){
+    if(nameCheck  && lastNameCheck && emailCheck && pass2Check && passCheck && idemPass){
     let usersLS = JSON.parse(localStorage.getItem('users'));
 
     if(usersLS.find(user=>user.email==email)){
-        console.log('no me hagas trampa');
+        console.log('no te veo');
     }else{
         usersLS.push(new User(usersLS.length+1,name,lastName,email,pass,false));
-        window.location.assign(window.location.origin + '/project 2 - rollingmovies/homepage.html')
+        window.location.assign(window.location.origin + '/homepage.html')
     }
 
     //! VER DE REDIRIGIR EL BOTON DE REGISTRO A HOME PAGE
 
     localStorage.setItem('users',JSON.stringify(usersLS))
     }else{
-    console.log('not available');
+    console.log('no existes');
     }
 
 } 
