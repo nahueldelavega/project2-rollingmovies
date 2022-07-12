5b4c783911081e9eedcf76ce542b6e12c994b0f8
Nahuel de la Vega <nahueldlvega@gmail.com>
2022-07-11 21:13:21 -0300

    cambios en js de contact

--- js/contact-page.js
+++ js/contact-page.js
@@ -15,38 +15,17 @@ const comments = (event) => {
       lastNameCheck &&
       emailCheck &&
       commentCheck &&
-    ) {
-      let usersLS = JSON.parse(localStorage.getItem("users"));
-  
-      if (usersLS.find((user) => user.email == email)) {
-        console.log("no pudimos validar tu identidad");
-      } else {
-        usersLS.push(
-          new User(usersLS.length + 1, name, lastName, email, pass, false)
+    ) {function emailSend(){
+      Email.send({
+          Host : "smtp.elasticemail.com",
+          Username : "nahueldlvega@gmail.com",
+          Password : "950D3D5AA996A669F9325F5D05E63B6692E3",
+          To : 'nahueldlvega@gmail.com',
+          From : email,
+          Subject : "Comentarios",
+          Body : comment
+      }).then(
+          message => alert(message)
           );
-          localStorage.setItem("userLogged", true)
-          localStorage.setItem("user", JSON.stringify(usersLS.length + 1));
-          window.location.assign(window.location.origin + "/homepage.html");
-        }
-        
-        localStorage.setItem("users", JSON.stringify(usersLS));
-      } else {
-        console.log("no puedes entrar aquí, pero si a la página de al lado");
-        incorrectAlert('Contraseña incorrecta.')
-    }
-    
-    
-    function emailSend(){
-        Email.send({
-            Host : "smtp.elasticemail.com",
-            Username : "nahueldlvega@gmail.com",
-            Password : "950D3D5AA996A669F9325F5D05E63B6692E3",
-            To : 'nahueldlvega@gmail.com',
-            From : "",
-            Subject : "Comentarios",
-            Body : ""
-        }).then(
-            message => alert(message)
-            );
-        }
+      }}    
     };
