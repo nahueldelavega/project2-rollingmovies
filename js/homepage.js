a873fc063ecbe46e6f6643360490ae146fc8f611
Gonzalo <gon01.dev@gmail.com>
2022-07-13 17:56:29 -0300

    Base terminada

--- js/homepage.js
+++ js/homepage.js
@@ -32,115 +32,185 @@ if (moviesFromLS) {
   moviesBase = moviesFromLS;
 } else {
   moviesBase = [
-    new Movie(
-      Math.floor(Math.random()*1000000),
-      "Movie1",
+    new Movie(1,
+      "The Prodigy",
       "Terror",
       "si",
       "si",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque arcu dolor. Cras ut ipsum sagittis, viverra erat in, maximus leo. Aenean fringilla ligula vel tincidunt pharetra. Vivamus accumsan egestas tortor sed tincidunt. Suspendisse eu felis et ipsum viverra elementum eu porta mi.",
-      "https://www.youtube.com/watch?v=jYRtFFa4hT8",
-      "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1459&q=80"
+      "El 22 de agosto de 2010 en la zona rural de Ohio, el asesino en serie Edward Scarka es asesinado a tiros durante una redada policial en su granja. En el momento de la muerte de Edward, la pareja casada Sarah y John Blume dan a luz a su hijo, Miles, en Pennsylvania . Miles muestra extrema sabiduría e inteligencia desde una edad temprana, y comienza a hablar con fluidez incluso antes de que sea un niño pequeño.",
+      "https://www.youtube.com/embed/HEeXv0x7a3s",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/poster-peliculas-terror-2019-prodigy-1578395572.jpg?crop=1xw:1xh;center,top&resize=480:*"
     ),
-    new Movie(
-      Math.floor(Math.random()*1000000),
-      "Movie2",
+    new Movie(2,
+      "El Rito",
+      "Terror",
+      "no",
+      "si",
+      "Michael Kovak es un decepcionado seminarista de Estados Unidos que decide asistir a un curso de exorcismos en el Vaticano. Es allí donde su fe empezará a tambalearse al tiempo que se enfrenta a terribles fuerzas demoníacas y al lado más oscuro de sus creencias.",
+      "https://www.youtube.com/embed/t5vA--Agb3U",
+      "https://graffica.info/wp-content/uploads/2015/10/El-rito-20111.jpg"
+    ),
+    new Movie(3,
+      "Tourist Trap",
+      "Terror",
+      "no",
+      "si",
+      "Un grupo de jóvenes turistas llegan a una casa en el medio del campo donde son recibidos por un viejo granjero. Sin embargo el aparentemente hospitalario buen hombre, es en realidad un psicópata que posee poderes telequinéticos",
+      "https://www.youtube.com/embed/sDpxEdIxxg4",
+      "https://i.pinimg.com/originals/53/95/be/5395be173f8f430e6c1933b3add99351.jpg"
+    ),
+    new Movie(4,
+      "La casa del terror",
+      "Terror",
+      "no",
+      "si",
+      "En Halloween, seis amigos deciden entrar en una casa encantada que promete ofrecer una experiencia extrema a base de explotar sus miedos más profundos. La noche se volverá mortal cuando se den cuenta de que algunos monstruos son reales.",
+      "https://www.youtube.com/embed/gtG1Y3_34ug",
+      "https://img.aullidos.com/imagenes/caratulas/casa-terror-sp.jpg"
+    ),
+    new Movie(5,
+      "Viernes 13",
+      "Terror",
+      "no",
+      "no",
+      "Wade (Jonathan Sadowski) les cuenta a sus amigos una historia de terror sobre un niño llamado Jason (Derek Mears), quien murió ahogado y ahora deambula por el bosque como un muerto en vida que castiga a los adolescentes irresponsables, pero nadie le cree. Esa noche, Jason aparece y asesina a los jóvenes.",
+      "https://www.youtube.com/embed/gz3MkaQ85i0",
+      "https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/cinefilia/mejores-posters-cine-terror-miedo/friday-4-poster/137670541-1-esl-ES/Friday-4-Poster.jpg"
+    ),
+    new Movie(6,
+      "IT",
+      "Terror",
+      "no",
+      "no",
+      "Cada 27 años aparece para secuestrar y matar niños. El grupo es atacado por el payaso, pero los niños consiguen huir. Los Perdedores llegan a la conclusión de que vive en la parte subterránea de una casa abandonada, el centro de la red de alcantarillado.",
+      "https://www.youtube.com/embed/fP4BBZ76DGg",
+      "http://www.paredro.com/wp-content/uploads/2016/10/te2.jpg"
+    ),
+    new Movie(7,
+      "El exorcista",
+      "Terror",
+      "no",
+      "si",
+      "La cinta relata los fatídicos hechos de la posesión diabólica de Regan MacNeil, una niña de doce años de edad, y del exorcismo al que más tarde fue sometida. Blatty, el autor de la novela, explicó que el argumento se inspiró en un hecho verídico sobre el que empezó a trabajar cuando aún era estudiante universitario.",
+      "https://www.youtube.com/embed/Q_Ytf1qGCTM",
+      "https://media.diariopopular.com.ar/p/02b96fe61702d5a46d9410867c500e19/adjuntos/143/imagenes/005/499/0005499617/posters-hw-38_resultjpg.jpg"
+    ),
+    new Movie(8,
+      "El renacido",
       "Drama",
       "si",
       "si",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque arcu dolor. Cras ut ipsum sagittis, viverra erat in, maximus leo. Aenean fringilla ligula vel tincidunt pharetra. Vivamus accumsan egestas tortor sed tincidunt. Suspendisse eu felis et ipsum viverra elementum eu porta mi.",
-      "https://www.youtube.com/watch?v=jYRtFFa4hT8",
-      "https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+      "Década de 1820. En una expedición al desconocido oeste norteamericano, todavía habitado por tribus indias, Hugh Glass (Leonardo DiCaprio), un trampero, explorador y cazador de pieles es brutalmente atacado por un gigante oso grizzly. El hombre resulta gravemente herido, y sus propios compañeros le dan por muerto.",
+      "https://www.youtube.com/embed/a8HRA49kaok",
+      "https://es.web.img3.acsta.net/pictures/15/11/04/17/06/413076.jpg"
     ),
-    new Movie(
-      Math.floor(Math.random()*1000000),
-      "Movie3",
-      "Comedia",
-      "no",
-      "no",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque arcu dolor. Cras ut ipsum sagittis, viverra erat in, maximus leo. Aenean fringilla ligula vel tincidunt pharetra. Vivamus accumsan egestas tortor sed tincidunt. Suspendisse eu felis et ipsum viverra elementum eu porta mi.",
-      "https://www.youtube.com/watch?v=jYRtFFa4hT8",
-      "https://images.unsplash.com/photo-1534684686641-05569203ecca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
-    ),
-    new Movie(
-      Math.floor(Math.random()*1000000),
-      "Movie3",
-      "Comedia",
+    new Movie(9,
+      "Titanic",
+      "Drama",
       "no",
       "si",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque arcu dolor. Cras ut ipsum sagittis, viverra erat in, maximus leo. Aenean fringilla ligula vel tincidunt pharetra. Vivamus accumsan egestas tortor sed tincidunt. Suspendisse eu felis et ipsum viverra elementum eu porta mi.",
-      "https://www.youtube.com/watch?v=jYRtFFa4hT8",
-      "https://images.unsplash.com/photo-1534684686641-05569203ecca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+      "La trama, una epopeya romántica,​​ relata la relación de Jack Dawson y Rose DeWitt Bukater, dos jóvenes que se conocen y se enamoran a bordo del transatlántico RMS Titanic en su viaje inaugural desde Southampton (Inglaterra) a Nueva York (Estados Unidos) en abril de 1912.",
+      "https://www.youtube.com/embed/wMZuro21wtE",
+      "https://i.pinimg.com/originals/fb/41/8e/fb418e89a9d058f1671d13f7a5afe6bd.jpg"
     ),
-    new Movie(
-      Math.floor(Math.random()*1000000),
-      "Movie3",
-      "Comedia",
+    new Movie(10,
+      "El padrino",
+      "Drama",
+      "si",
+      "si",
+      "Vito Corleone, cabeza de la familia más importante de la Cosa Nostra de Nueva York celebra por todo lo alto la boda de su hija Connie, debiendo atender las peticiones que le hacen ese día, por lo que recibe las visitas entre otros de Bonasera, dueño de una funeraria que pide vengue la violación de su hija.",
+      "https://www.youtube.com/embed/v72XprPxy3E",
+      "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/855/public/media/image/2017/11/20-mejores-peliculas-drama-historia-cine_4.jpg?itok=j2Sve7o0"
+    ),
+    new Movie(11,
+      "Dark crimes",
+      "Drama",
+      "no",
+      "no",
+      "Tadek (Jim Carrey) es un detective que se dispone a resolver un crimen. Un hombre de negocios ha sido asesinado y su muerte coincide con un crimen que se describe en la reciente novela publicada por Kozlow (Marton Csokas).",
+      "https://www.youtube.com/embed/NkMJPlenC3U",
+      "https://i.pinimg.com/564x/d2/c0/23/d2c0237f24de29b449459598b68a06a0.jpg"
+    ),
+    new Movie(12,
+      "Boyhood",
+      "Drama",
       "no",
       "si",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque arcu dolor. Cras ut ipsum sagittis, viverra erat in, maximus leo. Aenean fringilla ligula vel tincidunt pharetra. Vivamus accumsan egestas tortor sed tincidunt. Suspendisse eu felis et ipsum viverra elementum eu porta mi.",
-      "https://www.youtube.com/watch?v=jYRtFFa4hT8",
-      "https://images.unsplash.com/photo-1534684686641-05569203ecca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+      "BOYHOOD es la historia de Mason, desde que es un niño hasta que llega a la universidad, una historia llena de cambios: de ciudad de residencia, en la familia, de amistades,… “Un viaje íntimo y épico por la euforia de la niñez, los sísmicos cambios de una familia moderna y el paso del tiempo”3.",
+      "https://www.youtube.com/embed/fPCzKI8W5P8",
+      "https://i.pinimg.com/originals/25/2f/90/252f9073ef3845843d122786d8d63086.jpg"
     ),
-    new Movie(
-      Math.floor(Math.random()*1000000),
-      "Movie3",
-      "Comedia",
+    new Movie(13,
+      "No tengas miedo",
+      "Drama",
       "no",
       "si",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque arcu dolor. Cras ut ipsum sagittis, viverra erat in, maximus leo. Aenean fringilla ligula vel tincidunt pharetra. Vivamus accumsan egestas tortor sed tincidunt. Suspendisse eu felis et ipsum viverra elementum eu porta mi.",
-      "https://www.youtube.com/watch?v=jYRtFFa4hT8",
-      "https://images.unsplash.com/photo-1534684686641-05569203ecca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+      "Silvia es una joven de 25 años marcada por un oscuro pasado de abuso sexual infantil por parte de su entorno familiar. En la película se retratan de una manera muy dura las distintas etapas de la vida de Silvia desde su infancia hasta su juventud.",
+      "https://www.youtube.com/embed/NySWdWYZWwY",
+      "http://www.elseptimoarte.net/carteles/no_tengas_miedo_8434.jpg"
     ),
-    new Movie(
-      Math.floor(Math.random()*1000000),
-      "Movie3",
-      "Comedia",
-      "no",
-      "si",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque arcu dolor. Cras ut ipsum sagittis, viverra erat in, maximus leo. Aenean fringilla ligula vel tincidunt pharetra. Vivamus accumsan egestas tortor sed tincidunt. Suspendisse eu felis et ipsum viverra elementum eu porta mi.",
-      "https://www.youtube.com/watch?v=jYRtFFa4hT8",
-      "https://images.unsplash.com/photo-1534684686641-05569203ecca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
-    ),
-    new Movie(
-      Math.floor(Math.random()*1000000),
-      "Movie3",
-      "Comedia",
-      "no",
-      "si",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque arcu dolor. Cras ut ipsum sagittis, viverra erat in, maximus leo. Aenean fringilla ligula vel tincidunt pharetra. Vivamus accumsan egestas tortor sed tincidunt. Suspendisse eu felis et ipsum viverra elementum eu porta mi.",
-      "https://www.youtube.com/watch?v=jYRtFFa4hT8",
-      "https://images.unsplash.com/photo-1534684686641-05569203ecca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
-    ),
-    new Movie(
-      Math.floor(Math.random()*1000000),
-      "Movie3",
-      "Comedia",
-      "no",
-      "no",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque arcu dolor. Cras ut ipsum sagittis, viverra erat in, maximus leo. Aenean fringilla ligula vel tincidunt pharetra. Vivamus accumsan egestas tortor sed tincidunt. Suspendisse eu felis et ipsum viverra elementum eu porta mi.",
-      "https://www.youtube.com/watch?v=jYRtFFa4hT8",
-      "https://images.unsplash.com/photo-1534684686641-05569203ecca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
-    ),
-    new Movie(
-      Math.floor(Math.random()*1000000),
-      "Movie3",
+    new Movie(14,
+      "Un espía y medio",
       "Comedia",
       "si",
       "si",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque arcu dolor. Cras ut ipsum sagittis, viverra erat in, maximus leo. Aenean fringilla ligula vel tincidunt pharetra. Vivamus accumsan egestas tortor sed tincidunt. Suspendisse eu felis et ipsum viverra elementum eu porta mi.",
-      "https://www.youtube.com/watch?v=jYRtFFa4hT8",
-      "https://images.unsplash.com/photo-1534684686641-05569203ecca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+      "Un espía y medio cuenta la historia de un letal agente de la CIA (Johnson) con un pasado friki y víctima del acoso escolar, que vuelve a casa para asistir a una reunión de compañeros de instituto.",
+      "https://www.youtube.com/embed/eQnbwxzCzQ8",
+      "https://es.web.img2.acsta.net/pictures/16/05/31/13/33/072749.jpg"
     ),
-    new Movie(
-      Math.floor(Math.random()*1000000),
-      "Movie3",
+    new Movie(15,
+      "Delivery man",
       "Comedia",
       "no",
       "no",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque arcu dolor. Cras ut ipsum sagittis, viverra erat in, maximus leo. Aenean fringilla ligula vel tincidunt pharetra. Vivamus accumsan egestas tortor sed tincidunt. Suspendisse eu felis et ipsum viverra elementum eu porta mi.",
-      "https://www.youtube.com/watch?v=jYRtFFa4hT8",
-      "https://images.unsplash.com/photo-1534684686641-05569203ecca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+      "David Wozniak es un repartidor desafortunado para la carnicería de su familia, perseguidos por matones a quien le debe $ 80.000. Su novia Emma está embarazada de su hijo. Un día, David regresa del trabajo para encontrar un abogado en representación de un banco de esperma (donde dio 693 donaciones y obtuvo una suma de $ 24.255 durante sus años de estudiante), quien le dice que él era el padre de 533 hijos. De ellos, 142 se han unido a una acción de clase demanda para obligar a la clínica de fertilidad para revelar la identidad de 'Starbuck', el alias que había usado.",
+      "https://www.youtube.com/embed/yGAktL45XNQ",
+      "http://img5.allocine.fr/acmedia/medias/nmedia/19/00/03/09/20625190.jpg"
+    ),
+    new Movie(16,
+      "Free guy",
+      "Comedia",
+      "no",
+      "no",
+      "La película está protagonizada por Ryan Reynolds en el papel de un cajero de banco que descubre que en realidad es un personaje no jugador en un videojuego de mundo abierto y decide convertirse en el héroe de la historia y salvar a sus amigos de ser eliminados por el creador del juego.",
+      "https://www.youtube.com/embed/tZbNDmRGx_M",
+      "https://www.nacionrex.com/__export/1637002266150/sites/debate/img/2021/11/15/peliculas-de-comedia-2021-free-guy.jpg_486653160.jpg"
+    ),
+    new Movie(17,
+      "Noche de juegos",
+      "Comedia",
+      "no",
+      "si",
+      "La película está protagonizada por Ryan Reynolds en el papel de un cajero de banco que descubre que en realidad es un personaje no jugador en un videojuego de mundo abierto y decide convertirse en el héroe de la historia y salvar a sus amigos de ser eliminados por el creador del juego.",
+      "https://www.youtube.com/embed/oDcq4GBOaTg",
+      "https://imagenes.gatotv.com/categorias/peliculas/posters/noche_de_juegos.jpg"
+    ),
+    new Movie(18,
+      "Garra",
+      "Comedia",
+      "no",
+      "no",
+      "Stanley Sugerman es un cazatalentos internacional envejecido de los Los del 76 de Filadelfia Philadelphia 76ers de la Asociación Nacional de Baloncesto, que vive la vida en la carretera en busca de futuras estrellas. Sus frecuentes viajes afectan a su esposa Teresa y su hija Alex.",
+      "https://www.youtube.com/embed/lvRWUIUoaqU",
+      "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2022/05/poster-garra-2698329.jpg?itok=ekUigtuq"
+    ),
+    new Movie(19,
+      "...¿Y dónde estan las rubias?",
+      "Comedia",
+      "si",
+      "si",
+      "Dónde están la rubias comienza en una tienda donde dos agentes y hermanos del FBI, Kevin Copeland y Marcus Copeland intentan capturar a miembros de una organización que vende drogas dentro de cajas de helado, haciéndose pasar por oficinistas dominicanos.",
+      "https://www.youtube.com/embed/5_uNOf-abIQ",
+      "https://i.pinimg.com/originals/4d/56/19/4d5619d46fde30d04f7f456f1f9dc87d.jpg"
+    ),
+    new Movie(20,
+      "Jackass, la película",
+      "Comedia",
+      "no",
+      "si",
+      "Johnny Knoxville y sus compañeros bromistas pasean por Japón vestidos de pandas, causan estragos en campos de golf, hacen proezas con caimanes, y más. Ve todo lo que quieras. La primera de las películas inspiradas en la serie de MTV protagonizada por Johnny Knoxville, Steve-O y amigos.",
+      "https://www.youtube.com/embed/qynjN9xPh7Q",
+      "https://es.web.img2.acsta.net/pictures/14/07/24/10/14/072574.jpg"
     ),
   ];
   localStorage.setItem("moviesBase", JSON.stringify(moviesBase));
@@ -173,7 +243,7 @@ moviesFromLS.forEach((movie) => {
     active.innerHTML = `<div class="card text-white mt-0">
   <!-- <img src="${movie.urlImage}" class="d-block w-100 mainImage" alt="..." /> -->
   <iframe
-      src="${movie.urlVideo}"
+      src="${movie.urlVideo}?controls=0"
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
